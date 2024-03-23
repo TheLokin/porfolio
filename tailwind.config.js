3a729ee017f1c10a73e2a006b2b3c5c4574fe204
TheLokin <diego.ramil.lopez@gmail.com>
2024-03-23 13:31:11 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./src/**/*.{html,md,mdx,js,jsx,astro,ts,tsx,svelte,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ["Sora Variable", "system-ui", "sans-serif"],
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+    },
+  },
+  plugins: [
+    ({ addVariant }) => {
+      addVariant("any-hover", "@media (any-hover: hover) { &:hover }")
+    },
+  ],
+}
