d9667be6cf14378d94e95919dfd9b32b467f49aa
TheLokin <diego.ramil.lopez@gmail.com>
2024-04-04 00:35:31 +0200

    Add Vercel analytics

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,11 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/serverless"
 
 export default defineConfig({
+  output: "server",
   integrations: [tailwind()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 })
