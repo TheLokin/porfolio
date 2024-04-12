0672e0b4cd5295801422016be3686c8c1b2fc597
TheLokin <diego.ramil.lopez@gmail.com>
2024-04-12 15:18:11 +0200

    Refactor Astro configuration to import tailwind before other plugins

--- astro.config.js
+++ astro.config.js
@@ -1,5 +1,6 @@
-import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
+
+import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
 import sitemap from "@astrojs/sitemap"
 
