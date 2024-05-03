fcd7e9c9cd4ead5c2f66fe3a7256f492bcbc3153
TheLokin <diego.ramil.lopez@gmail.com>
2024-05-04 00:59:45 +0200

    Refactor Header.astro to add scrollbar-hide utility class

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,71 +18,111 @@ export default {
     },
   },
   plugins: [
-    ({ addComponents }) => {
+    ({ addUtilities, addComponents, addVariant }) => {
+      addUtilities(
+        {
+          ".scrollbar-hide": {
+            /* IE and Edge */
+            "-ms-overflow-style": "none",
+
+            /* Firefox */
+            "scrollbar-width": "none",
+
+            /* Safari and Chrome */
+            "&::-webkit-scrollbar": {
+              display: "none",
+            },
+          },
+
+          ".scrollbar-default": {
+            /* IE and Edge */
+            "-ms-overflow-style": "auto",
+
+            /* Firefox */
+            "scrollbar-width": "auto",
+
+            /* Safari and Chrome */
+            "&::-webkit-scrollbar": {
+              display: "block",
+            },
+          },
+        },
+        ["responsive"],
+      )
+
       addComponents({
-        ".typography-xs": /* Paragraph P3 */ {
+        /* Paragraph P3 */
+        ".typography-xs": {
           fontSize: "12px",
           lineHeight: "16px",
-          tracking: "2%",
         },
-        ".typography-sm": /* Paragraph P1 - Button B2 */ {
+
+        /* Paragraph P1 - Button B2 */
+        ".typography-sm": {
           fontSize: "16px",
           lineHeight: "20px",
-          tracking: "2%",
         },
-        ".typography-base": /* Heading H7 - Button B1 */ {
+
+        /* Heading H7 - Button B1 */
+        ".typography-base": {
           fontSize: "20px",
           lineHeight: "24px",
-          tracking: "2%",
         },
-        ".typography-lg": /* Heading H6 */ {
+
+        /* Heading H6 */
+        ".typography-lg": {
           fontSize: "16px",
           lineHeight: "20px",
-          tracking: "-2%",
         },
-        ".typography-xl": /* Heading H5 */ {
+
+        /* Heading H5 */
+        ".typography-xl": {
           fontSize: "20px",
           lineHeight: "24px",
-          tracking: "-2%",
         },
-        ".typography-2xl": /* Display (M) - Heading H4 */ {
+
+        /* Display (M) - Heading H4 */
+        ".typography-2xl": {
           fontSize: "24px",
           lineHeight: "28px",
-          tracking: "-2%",
         },
-        ".typography-3xl": /* Heading H3 */ {
+
+        /* Heading H3 */
+        ".typography-3xl": {
           fontSize: "28px",
           lineHeight: "32px",
-          tracking: "-2%",
         },
-        ".typography-4xl": /* Heading H2 */ {
+
+        /* Heading H2 */
+        ".typography-4xl": {
           fontSize: "32px",
           lineHeight: "40px",
-          tracking: "-2%",
         },
-        ".typography-5xl": /* Heading H1 */ {
+
+        /* Heading H1 */
+        ".typography-5xl": {
           fontSize: "36px",
           lineHeight: "44px",
-          tracking: "-2%",
         },
-        ".typography-6xl": /* Sub Heading */ {
+
+        /* Sub Heading */
+        ".typography-6xl": {
           fontSize: "40px",
           lineHeight: "48px",
-          tracking: "-2%",
         },
+
         ".typography-7xl": {
           fontSize: "44px",
           lineHeight: "52px",
-          tracking: "-2%",
         },
-        ".typography-8xl": /* Display */ {
+
+        /* Display */
+        ".typography-8xl": {
           fontSize: "48px",
           lineHeight: "56px",
-          tracking: "-2%",
         },
       })
-    },
-    ({ addVariant }) => {
+
       addVariant("any-hover", "@media (any-hover: hover) { &:hover }")
     },
   ],
