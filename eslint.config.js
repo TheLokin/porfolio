4efca615b56ebe6c674f24c3cb302f02e1b5b8bb
TheLokin <diego.ramil.lopez@gmail.com>
2024-11-27 02:56:42 +0100

    Add ESLint configuration for TypeScript, Astro, and Tailwind CSS

---
+++ eslint.config.js
@@ -0,0 +1,128 @@
+/* eslint-disable import/no-named-as-default-member */
+import eslintPluginAstro from "eslint-plugin-astro"
+import eslintPluginImport from "eslint-plugin-import"
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
+import eslintPluginTailwindcss from "eslint-plugin-tailwindcss"
+import tseslint from "typescript-eslint"
+
+export default [
+  // Ignore directories
+  {
+    ignores: ["dist", ".astro", ".vercel"],
+  },
+  // General configuration
+  {
+    rules: {
+      "padding-line-between-statements": [
+        "warn",
+        {
+          blankLine: "always",
+          prev: "*",
+          next: ["return", "break", "export"],
+        },
+        {
+          blankLine: "always",
+          prev: ["const", "let", "var"],
+          next: "*",
+        },
+        {
+          blankLine: "any",
+          prev: ["const", "let", "var"],
+          next: ["const", "let", "var"],
+        },
+      ],
+      "no-console": "warn",
+    },
+  },
+  // TypeScript configuration
+  ...[
+    ...tseslint.configs.recommended,
+    {
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          {
+            args: "after-used",
+            ignoreRestSiblings: false,
+            argsIgnorePattern: "^_.*?$",
+          },
+        ],
+      },
+    },
+    {
+      files: ["**/env.d.ts"],
+      rules: {
+        "@typescript-eslint/triple-slash-reference": "off",
+      },
+    },
+  ],
+  // Import configuration
+  ...[
+    eslintPluginImport.flatConfigs.recommended,
+    {
+      rules: {
+        "import/default": "off",
+        "import/named": "off",
+        "import/namespace": "off",
+        "import/no-unresolved": "off",
+        "import/order": [
+          "warn",
+          {
+            "groups": [
+              "type",
+              "builtin",
+              "object",
+              "external",
+              "internal",
+              "parent",
+              "sibling",
+              "index",
+            ],
+            "pathGroups": [
+              {
+                pattern: "~/**",
+                group: "external",
+                position: "after",
+              },
+            ],
+            "alphabetize": {
+              order: "asc",
+              caseInsensitive: true,
+            },
+            "newlines-between": "always",
+          },
+        ],
+      },
+      settings: {
+        "import/resolver": {
+          typescript: true,
+          node: true,
+        },
+      },
+    },
+  ],
+  // Astro configuration
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  // Tailwind CSS configuration
+  ...eslintPluginTailwindcss.configs["flat/recommended"],
+  // Prettier configuration
+  ...[
+    eslintPluginPrettier,
+    {
+      rules: {
+        "prettier/prettier": [
+          "warn",
+          {
+            tailwindFunctions: ["cva", "cn"],
+          },
+        ],
+      },
+    },
+    {
+      files: ["**/*.astro/*.js", "*.astro/*.js", "**/*.astro/*.ts", "*.astro/*.ts"],
+      rules: {
+        "prettier/prettier": "off",
+      },
+    },
+  ],
+]
