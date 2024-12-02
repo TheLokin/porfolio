b939b85fe553f9954ee20fc9187fc9b5893ce13e
TheLokin <diego.ramil.lopez@gmail.com>
2024-12-02 15:25:36 +0100

    Add ThemeToggle component with dark/light mode functionality and associated icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{html,md,mdx,js,jsx,astro,ts,tsx,svelte,vue}"],
   theme: {
     data: {
