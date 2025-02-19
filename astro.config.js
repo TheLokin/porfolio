c120dbf1d5306b55e558d26b2557747545849c3a
TheLokin <diego.ramil.lopez@gmail.com>
2025-02-19 20:30:32 +0100

    Remove robots.txt integration, add robots.txt file, and update site.webmanifest display property

--- astro.config.js
+++ astro.config.js
@@ -1,13 +1,12 @@
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel"
-import robotsTxt from "astro-robots-txt"
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
   site: "https://thelokin.dev",
   output: "static",
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap()],
   adapter: vercel({ webAnalytics: { enabled: true } }),
   i18n: {
     defaultLocale: "es",
